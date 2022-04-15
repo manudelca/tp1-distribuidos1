d2040fc9a63acf6981fdb26d30935651307cd334
manudelca <manueldelcarril@gmail.com>
2022-04-15 18:35:42 -0300

    Walking skeleton

---
+++ metric-server/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func InitConfig() (*viper.Viper, error) {
+	v := viper.New()
+
+	// Configure viper to read env variables
+	v.AutomaticEnv()
+	// Use a replacer to replace env variables underscores with points. This let us
+	// use nested configurations in the config file and at the same time define
+	// env variables for the nested configurations
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	// Add env variables supported
+	v.BindEnv("workers")
+	v.BindEnv("log", "level")
+
+	// Try to read configuration from config file. If config file
+	// does not exists then ReadInConfig will fail but configuration
+	// can be loaded from the environment variables so we shouldn't
+	// return an error in that case
+	v.SetConfigFile("./config.yaml")
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Printf("Configuration could not be read from config file. Using env variables instead")
+	}
+
+	return v, nil
+}
+
+// InitLogger Receives the log level to be set in logrus as a string. This method
+// parses the string and set the level to the logger. If the level string is not
+// valid an error is returned
+func InitLogger(logLevel string) error {
+	level, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		return err
+	}
+
+	logrus.SetLevel(level)
+	return nil
+}
+
+// PrintConfig Print all the configuration parameters of the program.
+// For debugging purposes only
+func PrintConfig(v *viper.Viper) {
+	logrus.Infof("Metric-server configuration")
+	logrus.Infof("Workers: %s", v.GetString("workers"))
+	logrus.Infof("Log Level: %s", v.GetString("log.level"))
+}
+
+func main() {
+	v, err := InitConfig()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	if err := InitLogger(v.GetString("log.level")); err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	// Print program config with debugging purposes
+	PrintConfig(v)
+}
