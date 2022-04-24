f4cad698985f1998b5b71092afdf83e3a1b0b55d
manudelca <manueldelcarril@gmail.com>
2022-04-23 23:31:01 -0300

    Event logging

--- metric-server/common/courier.go
+++ metric-server/common/courier.go
@@ -2,12 +2,20 @@ package common
 
 import (
 	"net"
+
+	"github.com/manudelca/tp1-distribuidos1/metric-server/protocol"
+	"github.com/sirupsen/logrus"
 )
 
 func HandleClientConnection(clientConn net.Conn) {
-	// event, err := protocol.GetEventFromMessage(clientConn)
-	// if err != nil {
-	// 	// What?
+	event, err := protocol.GetEventFromMessage(clientConn)
+	if err != nil {
+		logrus.Fatalf("[SERVER] Error trying to getEventFromMessage. Error: %s", err)
+	}
+	logrus.Infof("[SERVER] Event received %s", event)
+	// switch event.GetType() {
+	// case events.METRIC:
+	// case events.QUERY:
 	// }
 	clientConn.Close()
 }
