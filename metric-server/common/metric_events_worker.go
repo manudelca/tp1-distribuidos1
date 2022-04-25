d3ffec02acbc77b1b727f360ce80d732197002d9
manudelca <manueldelcarril@gmail.com>
2022-04-25 02:06:31 -0300

    Fixed endline

--- metric-server/common/metric_events_worker.go
+++ metric-server/common/metric_events_worker.go
@@ -24,10 +24,10 @@ func (m *MetricEventsWorker) ServeMetricEvents() {
 	layout := "2006-01-02 03:04:05"
 	for metricEvent := range m.metricEventsQueue {
 		logrus.Infof("[METRIC EVENTS WORKER] Processing metric event: ", metricEvent)
-		metricToWrite := fmt.Sprintf("%s %s %f", metricEvent.Date.Format(layout), metricEvent.MetricId, metricEvent.Value)
+		metricToWrite := fmt.Sprintf("%s %s %f\n", metricEvent.Date.Format(layout), metricEvent.MetricId, metricEvent.Value)
 		year, month, day := metricEvent.Date.Date()
 		hours, minutes, _ := metricEvent.Date.Clock()
-		fileToWrite := fmt.Sprintf("%s_%d%02d%02d_%02d%02d\n", metricEvent.MetricId, year, month, day, hours, minutes)
+		fileToWrite := fmt.Sprintf("%s_%d%02d%02d_%02d%02d", metricEvent.MetricId, year, month, day, hours, minutes)
 		err := m.fileMonitor.WriteLineOnFile(metricToWrite, fileToWrite)
 		if err != nil {
 			logrus.Infof("[METRIC EVENTS WORKER] Failed to write metric: \"%s\" On file: \"%s\". Error %s", metricToWrite, fileToWrite, err)
